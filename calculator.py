e3187546dc6b93ef04cf8f0848557bfa90699f15
janeyrinn <jane.rinn@gmail.com>
2021-12-06 10:00:24 +0000

    Add basic calculator functionality

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+num1 = int(input("num1: "))
+op = input("Operator: ")
+num2 = int(input("num2: "))
+
+if op == "+":
+     print(num1 + num2)
+elif op == "-":
+     print(num1 - num2)
+elif op == "/":
+     print(num1 / num2)
+elif op == "*":
+     print(num1 * num2)
+else:
+     print("Invalid Operator")
