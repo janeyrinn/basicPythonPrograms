9bcbed745e0bc66df7ac22e12b1a287f813fc180
janeyrinn <jane.rinn@gmail.com>
2021-12-06 12:50:53 +0000

    add encryption functionality

---
+++ encryptor.py
@@ -0,0 +1,15 @@
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                    translation = translation + "J"
+            else:
+                    translation = translation + "j"
+        else:
+            translation = translation + letter
+        return translation
+
+
+print(translate(input("Enter a phrase: ")))
